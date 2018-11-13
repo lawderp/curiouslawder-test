e2abcd3bc2cf6dd8465102a71befcb20b7632d9a
lawderp <40900898+lawderp@users.noreply.github.com>
2018-11-13 08:02:43 -0800

    Added build folder
    Let's see if this does the trick

---
+++ build/precache-manifest.323c9801434652d3fcb037a71ccc7ff5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "933094489dcb06e50097",
+    "url": "/static/js/main.93309448.chunk.js"
+  },
+  {
+    "revision": "af75269eeb1e100a999f",
+    "url": "/static/js/1.af75269e.chunk.js"
+  },
+  {
+    "revision": "933094489dcb06e50097",
+    "url": "/static/css/main.07ceedc3.chunk.css"
+  },
+  {
+    "revision": "af75269eeb1e100a999f",
+    "url": "/static/css/1.5650ef31.chunk.css"
+  },
+  {
+    "revision": "cc7d75b51079ada41fc4a0c78804aa82",
+    "url": "/index.html"
+  }
+];
